673d7485bb99df4fa64ffed78a810b66fdff1411
Tx-redcore <tonje_davidsen_98@hotmail.com>
2025-02-04 23:53:34 +0100

    Fixes for loading on other pc

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // API Routes
 
